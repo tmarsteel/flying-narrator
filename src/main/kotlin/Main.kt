439f89660f9608854356ee9170e3eea32c365ac4
Tobias Marstaller <tobias.marstaller@proton.me>
2026-04-10 14:49:49 +0200

    another pass of documenting reference severities on #10

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,11 @@ package io.github.tmarsteel.flyingnarrator
 
 import io.github.tmarsteel.flyingnarrator.easportswrc.EASportsWRCCleanGhostRouteReader
 import io.github.tmarsteel.flyingnarrator.feature.Feature
+import io.github.tmarsteel.flyingnarrator.feature.compoundRadius
 import io.github.tmarsteel.flyingnarrator.pacenote.derivePacenotes
 import java.nio.file.Paths
 import kotlin.io.path.readText
+import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
 
 fun main(args: Array<String>) {
@@ -12,6 +14,17 @@ fun main(args: Array<String>) {
         .read()
 
     val features = Feature.discoverIn(route)
+
+    features
+        .filterIsInstance<Feature.Corner>()
+        .forEach { f ->
+            val t = f.segments.maxOf { it.turnyness.absoluteValue }
+            val r = f.segments.compoundRadius
+            val d = f.segments.sumOf { it.arcLength }
+            val a = Math.toDegrees(f.totalAngle.absoluteValue)
+            println("$t;$r;$d;$a")
+        }
+
 
     features.derivePacenotes().forEach { (d, i) ->
         println("${d.roundToInt()}m: $i")
