b8588602c2b359b609861af822a80da822140779
Tobias Marstaller <tobias.marstaller@proton.me>
2026-04-16 15:28:56 +0200

    add failing test for StageProgressReporter

---
+++ src/test/kotlin/dirtrally2/StageProgressReporterTest.kt
@@ -0,0 +1,23 @@
+package io.github.tmarsteel.flyingnarrator.dirtrally2
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.doubles.shouldBeWithinPercentageOf
+import javax.imageio.ImageIO
+
+class StageProgressReporterTest : FreeSpec({
+    val reporter = StageProgressReporter()
+
+    "full game frame with only positive-delta splits" {
+        val frame = ImageIO.read(StageProgressReporterTest::class.java.getResourceAsStream("full_game_frame.png"))
+        val crop = reporter.getCropAreaForFrameSize(frame.width, frame.height)
+        val cropped = frame.getSubimage(crop.x, crop.y, crop.width, crop.height)
+        val progress = reporter.getProgressFromProgressIndicatorInGameFrame(cropped)
+        progress.shouldBeWithinPercentageOf(0.3218, 0.5)
+    }
+
+    "with negative-delta splits" {
+        val cropped = ImageIO.read(StageProgressReporterTest::class.java.getResourceAsStream("indicator_with_negative_delta_split.png"))
+        val progress = reporter.getProgressFromProgressIndicatorInGameFrame(cropped)
+        progress.shouldBeWithinPercentageOf(0.3218, 0.5)
+    }
+})
