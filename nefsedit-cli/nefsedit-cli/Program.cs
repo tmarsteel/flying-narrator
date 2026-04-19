9a16c27400be103596b2e455f10b87a8bc941eaa
Tobias Marstaller <tobias.marstaller@proton.me>
2026-04-19 18:30:36 +0200

    Add basic .NET project for EgoEngineModding code

---
+++ nefsedit-cli/nefsedit-cli/Program.cs
@@ -0,0 +1,16 @@
+﻿using EgoEngineLibrary.Language;
+using VictorBush.Ego.NefsLib;
+
+namespace nefsedit_cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            LngFile f = new LngFile(new FileStream(args[0], FileMode.Open));
+            Console.WriteLine(f.GetDataTable().Rows[0][0]);
+            Console.WriteLine(NefsVersionExtensions.ToPrettyString(NefsVersion.Version130));
+        }
+    }
+}
